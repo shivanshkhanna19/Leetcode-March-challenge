ffd2327083cfa9d8b1bf0e8f2f37962f95b7fa7c
Shivansh Khanna <53971327+shivanshkhanna19@users.noreply.github.com>
2024-09-24 01:50:14 +0530

    Create 242. Valid Anagram.cpp
    Valid Anagram.cpp

---
+++ 242. Valid Anagram.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+      
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        
+        for(int i=0;i<s.length();i++)
+        {
+            mp[s[i]]++;
+            
+            
+        }
+        
+        for(int i=0;i<t.length();i++)
+        {
+             mp[t[i]]--;
+            
+            if(mp[t[i]] < 0)
+            return false;
+            
+        }
+        return true;
+        
+        
+            
+    }
+};
